a8235a74319fac9a07d20929ffb1a1fe1ee381ff
Kubilay Özdemir <kubilayozdemir95@gmail.com>
2024-10-30 12:51:45 +0300

    Updated

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,7 +4,7 @@ import Home from "./pages/Home";
 import About from "./pages/About";
 import Dashboard from "./pages/Dashboard";
 import Projects from "./pages/Projects";
-import SignIn from "./pages/Signin";
+import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
