e9f50791928fc4f03128615b1da50765df58d4f2
Kubilay Özdemir <kubilayozdemir95@gmail.com>
2024-10-31 10:28:44 +0300

    Updated

--- client/src/pages/Search.jsx
+++ client/src/pages/Search.jsx
@@ -134,6 +134,7 @@ export default function Search() {
               <option value="reactjs">React.js</option>
               <option value="nextjs">Next.js</option>
               <option value="javascript">JavaScript</option>
+              <option value="Genel">Genel</option>
             </Select>
           </div>
           <Button type="submit" outline gradientDuoTone="purpleToPink">
