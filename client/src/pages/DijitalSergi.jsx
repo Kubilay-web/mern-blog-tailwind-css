8d75f4a5ace8731d6dde18c32460eed8f7e9f0b5
Kubilay Özdemir <kubilayozdemir95@gmail.com>
2024-11-09 16:58:50 +0300

    Updated

--- client/src/pages/DijitalSergi.jsx
+++ client/src/pages/DijitalSergi.jsx
@@ -1,29 +1,6 @@
 import { useState, useEffect } from "react";
 
 export default function DijitalSergi() {
-  const [posts, setPosts] = useState([]);
-  const [posts2, setPosts2] = useState([]);
-
-  const category = "Süreli Sergiler";
-  const category2 = "Koleksiyon Sergileri";
-
-  const fetchPosts = async (category, setPostFunc) => {
-    try {
-      const res = await fetch(
-        `/api/post/getposts/category?category=${category}`
-      );
-      const data = await res.json();
-      setPostFunc(data.posts);
-    } catch (error) {
-      console.error("Failed to fetch posts:", error);
-    }
-  };
-
-  useEffect(() => {
-    fetchPosts(category, setPosts);
-    fetchPosts(category2, setPosts2);
-  }, [category, category2]);
-
   return (
     <div>
       <style
@@ -46,35 +23,6 @@ export default function DijitalSergi() {
             </a>
           </div>
           {/*header rightside*/}
-          <div className="col-lg-4 col-md-5 d-none d-md-block align-self-center">
-            <div className="float-right ">
-              <a
-                className="float-left langField"
-                href="https://www.peramuseum.org/"
-                style={{ margin: "2px 0 0 0" }}
-              >
-                <img
-                  src="/images/DijitalSergi/iconLang.png"
-                  style={{ width: "25px", margin: "4px 0 0 0" }}
-                />
-              </a>
-              <div className="searchbar float-left">
-                <input
-                  className="search_input txtSrch1"
-                  id="srchBox"
-                  type="text"
-                  name="txtSrch1"
-                  placeholder="Arama..."
-                />
-                <a href="#" id="btnSrchDesktop" className="search_icon btnSrch">
-                  <img
-                    src="/images/DijitalSergi/iconSearch.png"
-                    style={{ width: "25px" }}
-                  />
-                </a>
-              </div>
-            </div>
-          </div>
         </div>
       </div>
       {/*
@@ -97,60 +45,6 @@ export default function DijitalSergi() {
       <div className="container">
         <div className="row">
           <div className="col-md-12"></div>
-        </div>
-      </div>
-
-      <div className="container">
-        <div className="row pt-7">
-          {posts && posts.length > 0 ? (
-            posts.slice(0, 1000).map((post, index) => {
-              let colClass = "col-lg-6 col-md-6"; // Varsayılan üçlü düzen
-
-              if (index === 0) {
-                // İlk kart 4'lük (yarım genişlik)
-                colClass = "col-lg-6";
-              } else if (index === 1) {
-                // İkinci kart 8'lik (tam genişlik)
-                colClass = "col-lg-6 col-md-12";
-              }
-
-              if (index === 2) {
-                // İlk kart 4'lük (yarım genişlik)
-                colClass = "col-lg-6";
-              } else if (index === 3) {
-                // İkinci kart 8'lik (tam genişlik)
-                colClass = "col-lg-6 col-md-12";
-              }
-
-              return (
-                <div key={post._id} className={`${colClass} mb-4`}>
-                  <a
-                    href={`/post/${post.slug}`}
-                    target="_self"
-                    className="no-link"
-                  >
-                    <div className="card mb-4 border-0">
-                      <img
-                        className="bd-placeholder-img card-img-top"
-                        width="100%"
-                        src={post.image}
-                        alt={post.altText}
-                      />
-                      <div className="card-body">
-                        <h3 className="card-title">{post.title}</h3>
-                        <p className="card-text">{post.content}</p>
-                        <div className="d-flex justify-content-between align-items-center">
-                          <small className="text-muted">{post.author}</small>
-                        </div>
-                      </div>
-                    </div>
-                  </a>
-                </div>
-              );
-            })
-          ) : (
-            <p>No posts available</p>
-          )}
         </div>
       </div>
 
