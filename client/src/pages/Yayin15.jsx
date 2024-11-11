f8feacb1e12f4fa1cc6b1d6ec874f95762d38b1e
Kubilay Özdemir <kubilayozdemir95@gmail.com>
2024-11-11 11:52:19 +0300

    Updated

--- client/src/pages/Yayin15.jsx
+++ client/src/pages/Yayin15.jsx
@@ -1,29 +1,4 @@
-import { useState, useEffect } from "react";
-
 const Yayin15 = () => {
-  const [posts, setPosts] = useState([]);
-  const [posts2, setPosts2] = useState([]);
-
-  const category = "Süreli Sergiler";
-  const category2 = "Koleksiyon Sergileri";
-
-  const fetchPosts = async (category, setPostFunc) => {
-    try {
-      const res = await fetch(
-        `/api/post/getposts/category?category=${category}`
-      );
-      const data = await res.json();
-      setPostFunc(data.posts);
-    } catch (error) {
-      console.error("Failed to fetch posts:", error);
-    }
-  };
-
-  useEffect(() => {
-    fetchPosts(category, setPosts);
-    fetchPosts(category2, setPosts2);
-  }, [category, category2]);
-
   return (
     <div>
       <style
@@ -78,60 +53,6 @@ const Yayin15 = () => {
         }}
       />
 
-      <div className="container">
-        <div className="row pt-7">
-          {posts && posts.length > 0 ? (
-            posts.slice(0, 1000).map((post, index) => {
-              let colClass = "col-lg-6 col-md-6"; // Varsayılan üçlü düzen
-
-              if (index === 0) {
-                // İlk kart 4'lük (yarım genişlik)
-                colClass = "col-lg-6";
-              } else if (index === 1) {
-                // İkinci kart 8'lik (tam genişlik)
-                colClass = "col-lg-6 col-md-12";
-              }
-
-              if (index === 2) {
-                // İlk kart 4'lük (yarım genişlik)
-                colClass = "col-lg-6";
-              } else if (index === 3) {
-                // İkinci kart 8'lik (tam genişlik)
-                colClass = "col-lg-6 col-md-12";
-              }
-
-              return (
-                <div key={post._id} className={`${colClass} mb-4`}>
-                  <a
-                    href={`/post/${post.slug}`}
-                    target="_self"
-                    className="no-link"
-                  >
-                    <div className="card mb-4 border-0">
-                      <img
-                        className="bd-placeholder-img card-img-top"
-                        width="100%"
-                        src={post.image}
-                        alt={post.altText}
-                      />
-                      <div className="card-body">
-                        <h3 className="card-title">{post.title}</h3>
-                        <p className="card-text">{post.content}</p>
-                        <div className="d-flex justify-content-between align-items-center">
-                          <small className="text-muted">{post.author}</small>
-                        </div>
-                      </div>
-                    </div>
-                  </a>
-                </div>
-              );
-            })
-          ) : (
-            <p>No posts available</p>
-          )}
-        </div>
-      </div>
-
       <div className="container first-row">
         <div className="row">
           <div className="col-md-12 ">
@@ -155,13 +76,13 @@ const Yayin15 = () => {
                   <a href="/yayin/osman-hamdi-bey-/234">
                     <img
                       className="bd-placeholder-img card-img-top"
-                      src="/images/Yayın15/osman-hamdi-bey-kucuk-kitap.jpg"
+                      src="/images/Yayın15/20-Dağcılık.webp"
                       alt="Osman Hamdi Bey "
                     />
                   </a>
                   <div className="card-body">
-                    <h3 className="card-title">Osman Hamdi Bey </h3>
-                    <p className="card-text">Bir Osmanlı Aydını </p>
+                    <h3 className="card-title">Dağcılık</h3>
+                    <p className="card-text">Dağcılık </p>
                   </div>
                 </div>
               </div>
@@ -171,15 +92,13 @@ const Yayin15 = () => {
                   <a href="/yayin/kahve-molasi/159">
                     <img
                       className="bd-placeholder-img card-img-top"
-                      src="/images/Yayın15/kahve-molasi.jpg"
+                      src="/images/Yayın15/21-Sosyal.webp"
                       alt="Kahve Molası"
                     />
                   </a>
                   <div className="card-body">
-                    <h3 className="card-title">Kahve Molası</h3>
-                    <p className="card-text">
-                      Kütahya Çini ve Seramiklerinde Kahvenin Serüveni{" "}
-                    </p>
+                    <h3 className="card-title">Sosyal</h3>
+                    <p className="card-text">Sosyal</p>
                   </div>
                 </div>
               </div>
@@ -189,15 +108,13 @@ const Yayin15 = () => {
                   <a href="/yayin/elgar-turkiye’de/157">
                     <img
                       className="bd-placeholder-img card-img-top"
-                      src="/images/Yayın15/elgar-turkiyede.jpg"
+                      src="/images/Yayın15/22-Kültür.jpg"
                       alt="Elgar Türkiye’de"
                     />
                   </a>
                   <div className="card-body">
-                    <h3 className="card-title">Elgar Türkiye’de</h3>
-                    <p className="card-text">
-                      İngiliz Bestecinin İstanbul ve İzmir Günleri
-                    </p>
+                    <h3 className="card-title">Kültür</h3>
+                    <p className="card-text">Kültür</p>
                   </div>
                 </div>
               </div>
@@ -207,15 +124,13 @@ const Yayin15 = () => {
                   <a href="/yayin/yusuf-agah-efendi-/143">
                     <img
                       className="bd-placeholder-img card-img-top"
-                      src="/images/Yayın15/yusuf-agah-efendi.jpg"
+                      src="/images/Yayın15/23-Pir Sultan.jpg"
                       alt="Yusuf Agâh Efendi "
                     />
                   </a>
                   <div className="card-body">
-                    <h3 className="card-title">Yusuf Agâh Efendi </h3>
-                    <p className="card-text">
-                      18. Yüzyıl Londrası’nda İlk Türk Büyükelçi
-                    </p>
+                    <h3 className="card-title">Pir Sultan </h3>
+                    <p className="card-text">Pir Sultan</p>
                   </div>
                 </div>
               </div>
